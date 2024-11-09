f9105adf90ecd154661e2c9ac2bba54d72cd5d3e
NeaEnot <oleg.kozloss@yandex.ru>
2024-11-09 21:30:15 +0400

    Модель заявки

---
+++ Src/Common/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    public class Order
+    {
+        public string Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Closed { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Price { get; set; }
+    }
+}
