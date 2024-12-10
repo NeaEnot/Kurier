ebd5d6b6bf9d2965e06003899adbd4a498551957
NeaEnot <oleg.kozloss@yandex.ru>
2024-12-11 01:45:44 +0400

    API Gateway

--- Src/Api/Program.cs
+++ Src/Api/Program.cs
@@ -1,3 +1,5 @@
+using Ocelot.DependencyInjection;
+
 namespace Kurier.Api
 {
     public class Program
@@ -6,18 +8,30 @@ namespace Kurier.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Configuration.AddJsonFile("ocelot.json");
+
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddOcelot();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+                app.MapGet("/swagger/v1/swagger.json", async (SwaggerAggregator aggregator) =>
+                {
+                    var aggregatedDoc = await aggregator.AggregateSwaggerDocsAsync();
+                    return Results.Json(aggregatedDoc);
+                });
+
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
@@ -25,7 +39,6 @@ namespace Kurier.Api
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
---
+++ Src/Api/SwaggerAggregator.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace Kurier.Api
+{
+    public class SwaggerAggregator
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly IConfiguration configuration;
+
+        public SwaggerAggregator(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.configuration = configuration;
+        }
+
+        public async Task<JObject> AggregateSwaggerDocsAsync()
+        {
+            var routes = configuration.GetSection("Routes");
+            var client = httpClientFactory.CreateClient();
+
+            var aggregatedPaths = new JObject();
+            var aggregatedComponents = new JObject();
+
+            foreach (var route in routes.GetChildren())
+            {
+                string protocol = route.GetSection("DownstreamScheme").Value;
+                string host = route.GetSection("DownstreamHostAndPorts").GetSection("Host").Value;
+                int port = int.Parse(route.GetSection("DownstreamHostAndPorts").GetSection("Port").Value);
+
+                var serviceSwagger = await client.GetStringAsync($"{protocol}://{host}:{port}/swagger/v1/swagger.json");
+                var serviceDoc = JObject.Parse(serviceSwagger);
+
+                if (serviceDoc["paths"] is JObject paths)
+                    foreach (var path in paths.Properties())
+                        aggregatedPaths[path.Name] = path.Value;
+
+                // Объединяем components (schemas в OpenAPI 3.0)
+                if (serviceDoc["components"]?["schemas"] is JObject schemas)
+                    foreach (var schema in schemas.Properties())
+                        aggregatedComponents[schema.Name] = schema.Value;
+            }
+
+            // Объединяем данные (упрощённый пример)
+            var aggregatedDoc = new JObject
+            {
+                ["openapi"] = "3.0.1",
+                ["info"] = new JObject
+                {
+                    ["title"] = "API Gateway",
+                    ["version"] = "v1"
+                },
+                ["paths"] = new JObject(aggregatedPaths["paths"]),
+                ["components"] = new JObject
+                {
+                    ["schemas"] = aggregatedComponents
+                }
+            };
+
+            return aggregatedDoc;
+        }
+    }
+}
